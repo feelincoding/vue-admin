7109235d9b6adaab5467477f8d755a660e8743a9
sloppy-cat <js3891129@gmail.com>
2022-06-09 13:10:24 +0900

    feat: 통계 > 서비스탭 화면 추가

--- src/views/main/monitoring/statistic/service/StaticticServicePage.vue
+++ src/views/main/monitoring/statistic/service/StaticticServicePage.vue
@@ -1,11 +1,74 @@
 <template>
   <div>
-    <h1>Statistic Service Page</h1>
+    <SelectSearch :isFocus="isSearchFocus" :propServiceList="searchSvcList" tab="service" @search="getSearchOption" />
+    <div
+      class="service-list"
+      v-if="serviceList != undefined && serviceList.svcStat != undefined && serviceList.svcStat.length > 0"
+    >
+      <ul>
+        <ServiceRow v-for="(item, index) in serviceList.svcStat" :key="index" :svcList="item" />
+      </ul>
+    </div>
+    <div class="service-list" v-else>
+      <div style="position: relative; text-align: center" v-if="isShowProgress">
+        <b-spinner label="Large Spinner"></b-spinner>
+      </div>
+      <div style="text-align: center" v-if="!isShowProgress">
+        <p>{{ emptyMsg }}</p>
+      </div>
+    </div>
   </div>
 </template>
-
 <script setup lang="ts">
-</script>
+import ErrorCode from '@/error/ErrorCodes';
+import MonitoringStatisticRepository from '@/repository/monitoring-statistic-repository';
+import type { StatResponse } from '@/types/MonitoringStatisticType';
+import { ref, reactive, computed, watch, onMounted, type Ref } from 'vue';
+const props = defineProps<{
+  isSearchFocus: boolean;
+}>();
 
-<style scoped>
-</style>
+const statRepository = new MonitoringStatisticRepository();
+let emptyMsg = ref('데이터를 조회해주세요.');
+
+let isShowProgress = ref(false);
+
+let serviceList: Ref<StatResponse | null> = ref(null);
+let searchSvcList: Ref<string[]> = ref([]);
+
+onMounted(() => {
+  if (props.isSearchFocus) {
+    statRepository.getSearchSvcList('');
+  }
+});
+
+watch(serviceList, () => {
+  if (
+    serviceList.value === null ||
+    (serviceList.value != null && (serviceList.value.svcStat === undefined || serviceList.value.svcStat.length === 0))
+  ) {
+    emptyMsg.value = '조회 가능한 데이터가 없습니다.';
+  } else if (serviceList.value === undefined) {
+    emptyMsg.value = '데이터를 조회해주세요.';
+  } else {
+    emptyMsg.value = '';
+  }
+});
+
+function getSearchOption(event: any) {
+  isShowProgress.value = true;
+  statRepository
+    .getServiceList(event)
+    .then(() => {
+      isShowProgress.value = false;
+    })
+    .catch((err) => {
+      if (err.getErrorCode() == ErrorCode.CANCEL_ERROR) {
+        console.log('SYSTEM API Cancel');
+      } else {
+        isShowProgress.value = false;
+        // this.$modal.show(`${this.$t('error.server_error')}`);
+      }
+    });
+}
+</script>
