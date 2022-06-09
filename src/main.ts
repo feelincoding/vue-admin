e56fab828655e4f338e1fd07189982cc440c1dd5
sloppy-cat <js3891129@gmail.com>
2022-06-09 14:28:17 +0900

    feat: vue-datepicker 라이브러리 추가

--- src/main.ts
+++ src/main.ts
@@ -26,11 +26,15 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 //   },
 // });
 
+// import Datepicker
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(BootstrapVue3);
 app.use(router);
 app.use(i18n);
-
+app.component('Datepicker', Datepicker);
 app.mount('#app');
