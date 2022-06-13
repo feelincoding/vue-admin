564cf3668e62ebc45667b6cbd852f3bd6a32d088
feelincoding <feelincoding@gmail.com>
2022-06-13 10:18:21 +0900

    add: system 끝

---
+++ src/views/main/system-mngt/edit/SystemEditPage.vue
@@ -0,0 +1,177 @@
+<template>
+  <ContentLayout
+    :isShowProgress="isShowProgress"
+    :title="$t('system.modify_top_title')"
+    :subTitle="$t('system.modify_cont_title')"
+    :depth="$t('system.modify_depth')"
+  >
+    <template v-if="!isShowProgress" v-slot:contents>
+      <ul>
+        <InputGroup
+          type="text"
+          :value.sync="systemItem.id"
+          :isValid.sync="idValid"
+          :inputNm="$t('system.id')"
+          :place="$t('system.id_placeholder')"
+          :disabled="true"
+          :required="true"
+        />
+
+        <EdptForm
+          :inputNm="$t('system.edpt')"
+          :strArr.sync="systemItem.edpt"
+          :isValid.sync="edptValid"
+          :editPage="true"
+        />
+        <InputGroup
+          type="text"
+          :value.sync="systemItem.tkcgrNm"
+          :isValid.sync="tkcgrNmValid"
+          :inputNm="$t('system.tkcgrNm')"
+          :place="$t('system.tkcgrNm')"
+        />
+
+        <InputGroup
+          type="text"
+          :value.sync="systemItem.tkcgrPos"
+          :inputNm="$t('system.tkcgrPos')"
+          :place="$t('system.tkcgrPos')"
+          :isValid.sync="tkcgrPosValid"
+        />
+        <InputGroup
+          type="email"
+          :value.sync="systemItem.tkcgrEml"
+          :inputNm="$t('system.tkcgrEml')"
+          :place="$t('system.tkcgrEml')"
+          :isValid.sync="tkcgrEmlValid"
+        />
+        <TextAreaGroup :inputNm="$t('system.desc')" :value.sync="systemItem.desc" />
+      </ul>
+      <ModalLayout size="s" v-if="isShowModal">
+        <template v-slot:modalHeader
+          ><h1 class="h1-tit">{{ $t('system.modal_system_edit') }}</h1>
+        </template>
+        <template v-slot:modalContainer>
+          <p class="text">{{ $t('system.modal_edit_message') }}</p>
+        </template>
+        <template v-slot:modalFooter
+          ><button class="lg-btn purple-btn" @click="onSubmit">{{ $t('common.ok') }}</button
+          ><button class="lg-btn white-btn" @click="closeModal">{{ $t('common.cancel') }}</button>
+        </template>
+      </ModalLayout>
+    </template>
+    <template v-slot:buttons v-if="!isShowProgress">
+      <div class="btn-wrap">
+        <button class="lg-btn purple-btn" @click="showModal" :disabled="isBtnDisabled">
+          {{ $t('common.modify') }}
+          <b-spinner variant="light" label="Spinning" v-if="isBtnDisabled" small></b-spinner>
+        </button>
+        <button class="lg-btn white-btn" @click="cancelOnClickEvent" :disabled="isBtnDisabled">
+          {{ $t('common.cancel') }}
+        </button>
+      </div>
+    </template>
+  </ContentLayout>
+</template>
+<script setup lang="ts">
+import type { SystemResponse } from '@/types/SystemType';
+
+import SystemModule from '@/stores/modules/SystemModule';
+
+import ContentLayout from '@/components/layout/ContentLayout.vue';
+import InputGroup from '@/components/system-mngt/InputGroup.vue';
+import TextAreaGroup from '@/components/system-mngt/TextAreaGroup.vue';
+import EdptForm from '@/components/system-mngt/EdptForm.vue';
+import ModalLayout from '@/components/commons/modal/ModalLayout.vue';
+
+import { ref, reactive, computed, watch, onMounted, inject } from 'vue';
+import type { PropType } from 'vue';
+import type { Ref } from 'vue';
+
+import { BSpinner } from 'bootstrap-vue-3';
+
+import { useRoute } from 'vue-router';
+import router from '@/router';
+import { modalInjectionKey } from '@/plugins/modal/ModalPlugin';
+import { useI18n } from 'vue-i18n';
+import { useToast } from 'vue-toastification';
+const toast = useToast();
+const { t } = useI18n({});
+const route = useRoute();
+const modal = inject(modalInjectionKey)!!;
+
+const systemModule = new SystemModule();
+
+const systemItem = ref<SystemResponse>({} as SystemResponse);
+
+const idValid = ref(true);
+const tkcgrNmValid = ref(true);
+const tkcgrPosValid = ref(true);
+const tkcgrEmlValid = ref(true);
+const edptValid = ref(true);
+
+const isShowProgress = ref(false);
+const isShowModal = ref(false);
+const isBtnDisabled = ref(false);
+
+// get system() {
+//   return this.systemModule.system;
+// }
+
+onMounted(() => {
+  isShowProgress.value = true;
+
+  systemModule
+    .getSystemDetail(route.params.id as string)
+    .then((res) => {
+      isShowProgress.value = false;
+
+      systemItem.value = res;
+    })
+    .catch(() => {
+      isShowProgress.value = false;
+      modal().show(t('error.server_error'));
+    });
+});
+
+// @Watch('system')
+// onSystemChange() {
+//   this.systemItem = this.system as SystemResponse;
+// }
+
+const showModal = () => {
+  const val =
+    idValid.value && tkcgrNmValid.value && tkcgrPosValid.value && tkcgrEmlValid.value && edptValid.value ? true : false;
+
+  if (!val) {
+    modal().show(t('system.empty_check_message'));
+    return;
+  } else {
+    isShowModal.value = true;
+  }
+};
+
+const closeModal = () => {
+  isShowModal.value = false;
+};
+
+const onSubmit = async () => {
+  isBtnDisabled.value = true;
+  isShowModal.value = false;
+  systemModule
+    .updateSystemDetail(systemItem.value)
+    .then(() => {
+      isShowProgress.value = false;
+      router.back();
+    })
+    .catch(() => {
+      isShowProgress.value = false;
+      modal().show(t('error.server_error'));
+    });
+};
+
+const cancelOnClickEvent = () => {
+  router.go(-1);
+};
+</script>
+<style lang=""></style>
