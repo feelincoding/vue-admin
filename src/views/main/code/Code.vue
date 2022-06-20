f2e46a3743279b02e576ed0ccd146ba1e97a2d00
chance-up <ccs6473@naver.com>
2022-06-20 17:17:56 +0900

    feat:code작성 라이브러리(Monaco) 적용

--- src/views/main/code/Code.vue
+++ src/views/main/code/Code.vue
@@ -1,7 +1,38 @@
-<template></template>
+<template>
+  <div id="container" style="height: 1000px">
+    <h1 class="h1-tit">핸들러 코드 작성</h1>
+    <p></p>
+    <div id="editor-section" style="height: 80%"></div>
+  </div>
+</template>
 
 <script setup lang="ts">
 import { ref, reactive, computed, watch, onMounted, inject } from 'vue';
+import * as monaco from 'monaco-editor';
+
+const codeEditor = ref();
+
+function initEditor() {
+  codeEditor.value = monaco.editor.create(document.getElementById('editor-section') as HTMLDivElement, {
+    value: code.value,
+    language: 'java',
+    theme: 'vs-dark',
+    fontSize: 18,
+    lineNumbers: 'on',
+    minimap: {
+      enabled: false,
+    },
+  });
+}
+
+function runCode() {
+  console.log('runCode');
+  console.log(codeEditor.value.getValue());
+}
+
+onMounted(() => {
+  initEditor();
+});
 
 const code = ref(`package com.ktds.act.apigw.filter.handler.request.convertBody;
 
@@ -23,7 +54,7 @@ public class RequestConvertBodyHandler implements RouteHandler {
 	@Override
 	public boolean process(TransactionDto transaction) {
 		log.info("REQ.CONVERT-BODY - Start");
-		
+
 		// if(transaction.getRequestData().getBody()!=null){
 		// 	log.info("요청 바디 : " + new String(transaction.getRequestData().getBody(), Charset.forName("UTF-8")));
 		// 	log.info("바디 정보 변경 : request");
@@ -34,5 +65,5 @@ public class RequestConvertBodyHandler implements RouteHandler {
 	}
 }`);
 
-onMounted(() => {});
+// onMounted(() => {});
 </script>
