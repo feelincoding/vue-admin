9d72f6649b058f9e72467b08683b7694c01c61bf
sloppy-cat <js3891129@gmail.com>
2022-06-21 15:40:50 +0900

    fix: 주석 추가

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,6 +1,7 @@
 <template>
   <article class="body-cont">
     <h1>this is LoginPage!!</h1>
+    <!-- 리캡차 사용 -->
     <vue-recaptcha
       @verify="handleSuccessRecaptcha"
       @expired="handleFailRecaptcha"
@@ -14,14 +15,17 @@
 <script setup lang="ts">
 import { VueRecaptcha } from 'vue-recaptcha';
 import i18n from '@/plugins/Locale';
+
+//Todo 아래의 recaptcha 키는 개인발급된 키입니다. 변경 필요합니다.
 const sitekey = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
+
+// 리캡차 통과, 로그인 시나리오 진행
 const handleSuccessRecaptcha = (token: string) => {
-  // 리캡차 통과, 로그인 시나리오 진행
   console.log('리캡차 통과\n token : ', token);
 };
 
+// 리캡차 실패
 const handleFailRecaptcha = (error: any) => {
-  // 리캡차 실패, 시나리오 정상 진행
-  console.log('리캡차 실패 : ', error);
+  console.log('리캡차 실패\n', error);
 };
 </script>
