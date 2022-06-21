28d5e2cbb8204fb505c9bb30ebeba75791928cf8
sloppy-cat <js3891129@gmail.com>
2022-06-21 15:09:55 +0900

    feat: recaptcha3 => 2 변경, recaptcha key 추가

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
+  <article class="body-cont">
     <h1>this is LoginPage!!</h1>
-  </div>
+    <vue-recaptcha
+      @verify="handleSuccessRecaptcha"
+      @expired="handleFailRecaptcha"
+      @error="handleFailRecaptcha"
+      :sitekey="sitekey"
+      language="ko"
+    ></vue-recaptcha>
+  </article>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { VueRecaptcha } from 'vue-recaptcha';
+import i18n from '@/plugins/Locale';
+const sitekey = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
+const handleSuccessRecaptcha = (token: string) => {
+  // 리캡차 통과, 로그인 시나리오 진행
+  console.log('리캡차 통과\n token : ', token);
+};
 
-<style scoped></style>
+const handleFailRecaptcha = (error: any) => {
+  // 리캡차 실패, 시나리오 정상 진행
+  console.log('리캡차 실패 : ', error);
+};
+</script>
